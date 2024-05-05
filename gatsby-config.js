5a17c2bebebca05a4dfaef73a1d1d7dec0238652
Mitchell Tyson <mrtyson@amazon.com>
2024-05-05 13:38:37 -0400

    remove test for cicd test

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Final Project - Mitchell Tyson Test`,
+    title: `Final Project - Mitchell Tyson`,
     description: `Final Project Notes App with custom API`,
     course: `CSCI E-114`,
     siteUrl: `http://localhost:8000`,
